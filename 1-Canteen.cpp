305d3a44458f8fbdd05fe5752ca1be74e317b1e6
Aiidanaqq <150505764+Aiidanaqq@users.noreply.github.com>
2023-11-18 21:22:00 +0600

    homework

--- 1-Canteen.cpp
+++ 1-Canteen.cpp
@@ -1,1 +1,15 @@
-hello 
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int A, B, N;
+    cin >> A >> B >> N;
+
+    int totalCostSom = A * N;
+    int totalCostTyiyn = B * N;
+
+    cout << totalCostSom << " som and " << totalCostTyiyn << " tyiyn." << endl;
+
+    return 0;
+}
